2edf4dc3c8c6e37fe62cfe510b445e761264cac2
Jared Tebbi <tebbij6@students.rowan.edu>
2020-11-04 16:31:45 -0500

    fix missing connection to data service

--- frontend/recipe-recommender/src/app/my-recipes/my-recipes.component.ts
+++ frontend/recipe-recommender/src/app/my-recipes/my-recipes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../data.service';
+
 @Component({
   selector: 'app-my-recipes',
   templateUrl: './my-recipes.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyRecipesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
   
   recipes = [];
 
