0893218f43dad14a3ddcad4c7a339a4f1f9fa6c9
PoundofFlesh <joshhric@gmail.com>
2020-11-12 08:22:09 -0500

    package

--- backend/README.md
+++ backend/README.md
@@ -14,3 +14,11 @@ Node_Modules is huge so I gitignored it. So you should run 'npm install' after c
 Start the server with node app.js or nodemon.js (npm install nodemon, will restart server whenever you make a change)
 
 Go to webpage (ng serve -o) and navigate to take a survey page. Searching for any of the 5 recipes up top will display data provided by mysql.
+
+
+To test the JWT:
+    1) npm i jsonwebtoken dotenv
+    2) npm run devStart
+    3) Use REST functionality ("Rest Client" exstention (0.24.4 or higher), Postman, etc.) to test GET and POST calls
+        a) Use the post call to get the udpated token
+        b) Replace the token with old token next to "Bearer" in the get request
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -12,14 +12,14 @@ const db = mysql.createConnection({
     database: 'db'
 })
 
-db.connect((err) => {
+/*db.connect((err) => {
     if (err) {
         throw err;
     }
     console.log("Database Connected");
-});
+});*/
 
-router.get('/recipes/:recipe_name/search', function(req, res) {
+/*router.get('/recipes/:recipe_name/search', function(req, res) {
     var recipe_name = req.params.recipe_name;
     console.log(req.params.recipe_name)
     let sql = "SELECT * FROM recipe WHERE recipe_name = '" + recipe_name + "'";
@@ -30,6 +30,6 @@ router.get('/recipes/:recipe_name/search', function(req, res) {
         console.log(results);
         res.send(results);
     })
-})
+})*/
 
 module.exports = router;
