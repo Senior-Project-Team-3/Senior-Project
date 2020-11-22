a70e258ee6ae268a7992b97234d662c57b8db22f
idoyle19 <doylei19@students.rowan.edu>
2020-11-21 22:11:14 -0500

    commenetd out fetch stuff, cookies can be set

--- frontend/recipe-recommender/src/app/data.service.ts
+++ frontend/recipe-recommender/src/app/data.service.ts
@@ -23,11 +23,11 @@ export class DataService {
   }
 
   public putSurveyResults(results: String, userID: number) {
-    return this.httpClient.put(this.REST_API_SERVER + '/survey_results/' + userID, {"data": results});
+    return this.httpClient.put(this.REST_API_SERVER + '/survey_results/' + userID, {"data": results}, { withCredentials: true});
   }
 
   public getMyRecipes(userID: String) {
     console.log(userID);
-    return this.httpClient.get(this.REST_API_SERVER + '/recipes/' + userID + '/my_recipes');
+    return this.httpClient.get(this.REST_API_SERVER + '/recipes/' + userID + '/my_recipes', { withCredentials: true});
   }
 }
