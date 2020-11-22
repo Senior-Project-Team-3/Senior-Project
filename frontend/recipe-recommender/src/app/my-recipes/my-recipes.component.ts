91cbdecf3a682f52db051faa124dd44a0bc2f217
Jared Tebbi <tebbij6@students.rowan.edu>
2020-11-22 14:48:55 -0500

    Change to myRecipes to make it display recipe info

--- frontend/recipe-recommender/src/app/my-recipes/my-recipes.component.ts
+++ frontend/recipe-recommender/src/app/my-recipes/my-recipes.component.ts
@@ -29,10 +29,9 @@ export class MyRecipesComponent implements OnInit {
     console.log("test");
     this.dataService.getMyRecipes('38094').subscribe((data: any[]) => {
       console.log(data);
-      console.log("test");
       console.log(data[0]);
-      this.recipes = data;
-      console.log(Math.ceil(this.recipes.length/3));
+      this.recipes = data[0];
+      //console.log(Math.ceil(this.recipes.length/3));
       console.log(this.recipes);
       for (let index = 0; index < (3 - (this.recipes.length % 3) + this.recipes.length)/3 ; index++) {
         this.recipesSmall.push("test");
