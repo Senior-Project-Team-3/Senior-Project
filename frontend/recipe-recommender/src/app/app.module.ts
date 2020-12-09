6cf0f893989594b443c25c3d70252bd033b1a703
Jared Tebbi <tebbij6@students.rowan.edu>
2020-12-09 18:52:34 -0500

    Resolved merge conflict

--- frontend/recipe-recommender/src/app/app.module.ts
+++ frontend/recipe-recommender/src/app/app.module.ts
@@ -15,11 +15,8 @@ import { MyRecipesComponent } from './my-recipes/my-recipes.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { ReviewComponent } from './review/review.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { SearchResultsComponent } from './search-results/search-results.component';
-=======
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
->>>>>>> 13ef2cc2b6bbfdbe84f12e2b917822a142e42532
 
 @NgModule({
   declarations: [
